70943a9d4c9d754a6d931421c8adaec9a8eb6332
Amirhousain110 <85882311+Amirhousain110@users.noreply.github.com>
2025-10-20 00:38:33 +0330

    add installer

---
+++ installer.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+conf=#HOME/.config/nvim
+dfile=https://github.com/Amirhousain110/NiflHeim.git
+
+CYAN='\033[0;36m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+pkg_install() {
+
+	local distro
+	local cmd
+	local usesudo
+	declare -A pkgmgr
+	pkgmgr=(
+		[arch]="pacman -S"
+		[alpine]="apk add"
+		[debian]="apt-get install "
+		[ubuntu]="apt-get install "
+	)
+
+	distro=$(cat /etc/os-release | tr [:upper:] [:lower:] | grep -Poi '(debian|ubuntu|red hat|centos|arch|alpine)' | uniq)
+	cmd="${pkgmgr[$distro]}"
+	[[ ! $cmd ]] && return 1
+	if [[ $1 ]]; then
+		[[ ! $EUID -eq 0 ]] && usesudo=sudo
+		echo installing packages command: $usesudo $cmd $@
+		$usesudo $cmd $@
+	else
+		echo $cmd
+	fi
+}
+
+echo -e "${CYAN}"
+echo "███╗   ██╗██╗███████╗██╗     ██╗  ██╗███████╗██╗███╗   ███╗"
+echo "████╗  ██║██║██╔════╝██║     ██║  ██║██╔════╝██║████╗ ████║"
+echo "██╔██╗ ██║██║█████╗  ██║     ███████║█████╗  ██║██╔████╔██║"
+echo "██║╚██╗██║██║██╔══╝  ██║     ██╔══██║██╔══╝  ██║██║╚██╔╝██║"
+echo "██║ ╚████║██║██║     ███████╗██║  ██║███████╗██║██║ ╚═╝ ██║"
+echo "╚═╝  ╚═══╝╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝     ╚═╝"
+echo -e "${NC}"
+echo "Welcom to NiflHeim Installer."
+
+read -p "Do you want to continue? (Y/n): " choice1
+read -p "Do you whant to install required dependencies? (Y/n): " depChoice
+
+if [[ $depChoice == "y" || $depChoice == "Y" || $depChoice == "" ]]; then
+	echo "Installing dependencies..."
+	pkg_install "fzf lazygit ripgrep fd tree-sitter tree-sitter-c tree-sitter-cli neovim git curl nodejs gcc"
+
+else
+	echo "Skipping dependency installation..."
+fi
+
+if [[ $choice1 == "y" || $choice1 == "Y" || $choice1 == "" ]]; then
+	echo "Installing NiflHeim..."
+
+	read -p "Do you whant to back up your current Neovim configuration? (Y/n):" choice2
+	if [[ $choice2 == "y" || $choice2 == "Y" || $choice2 == "" ]]; then
+		echo "Backing up current configuration..."
+
+		confBack=${conf}_$(date +%F_%T).bak
+
+		mv $conf $confBack
+		echo "Backup created at $confBack"
+
+		rm -rf $HOME/.local/share/nvim
+		rm -rf $HOME/.local/state/nvim
+		rm -rf $HOME/.cache/nvim
+		git clone $dfile $conf
+
+	else
+		echo "Skipping backup..."
+
+		rm -rf $conf
+		rm -rf $HOME/.local/share/nvim
+		rm -rf $HOME/.local/state/nvim
+		rm -rf $HOME/.cache/nvim
+		git clone $dfile $conf
+	fi
+
+	echo -e "${GREEN}NiflHeim installed successfully!${NC}"
+
+	read -p "Do you want to install NiflHeim on your root user?(Y/n)" choiceRoot
+	if [[ $choiceRoot == "y" || $choiceRoot == "Y" || $choiceRoot == "" ]]; then
+
+		sudo true
+		rm -rf /root/.config/nvim
+		rm -rf /root/.local/share/nvim
+		rm -rf /root/.local/state/nvim
+		rm -rf /root/.cache/nvim
+		git clone $dfile /root/.config/nvim
+		echo -e "${GREEN}NiflHeim installed successfully for root user!${NC}"
+
+	else
+		echo -e "${CYAN}Skipping root user installation.${NC}"
+		exit 0
+	fi
+else
+	echo -e "${RED}Installation cancelled.${NC}"
+	exit 0
+fi
