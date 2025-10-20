ffa65af5eaa74bed3d634bc53c1efdbdc43c3d39
Amirhousain110 <amir.gh.84110@gmail.com>
2025-10-20 06:39:02 +0330

    change Cmp border

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,17 @@
+--added rounded borders to cmp documentation window
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      menu = {
+        border = "rounded",
+        winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+      },
+      documentation = {
+        window = {
+          border = "rounded",
+        },
+      },
+    },
+  },
+}
